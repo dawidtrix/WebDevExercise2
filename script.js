bde452247cb3dc122e180e5bdc058b82de84f0c8
dawidtrix <139803338+dawidtrix@users.noreply.github.com>
2024-05-28 10:17:48 +0200

    e

---
+++ script.js
@@ -0,0 +1,19 @@
+function PopupMonster() {
+    let name = prompt("Hello! Vad heter du?");
+    alert("Nice to meet you " + name);
+    let age = prompt("Hur gammal är du?");
+    if(age < 18){
+        alert("Grow up!");
+    }
+    else {
+        alert("Welcome to my nice webpage.");
+    }
+    let cool = confirm("So " + name + ", are you hot enough to be cool?")
+    
+    if(cool){
+        alert("Yeeeeaah!")
+    }
+    else {
+        alert("Bad day, uh?");
+    }
+}
